67e266b96e37b48cada0a45f91b7b324c08b3184
kimck49 <kimck49@naver.com>
2022-05-09 16:04:01 +0900

    컨테이너 설정파일 연습 예제

---
+++ src/main/java/com/gyojincompany/spring_6_1/StudentInfo.java
@@ -0,0 +1,22 @@
+package com.gyojincompany.spring_6_1;
+
+public class StudentInfo {
+	
+	private Student student;	
+	
+	public StudentInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+	
+	
+}
